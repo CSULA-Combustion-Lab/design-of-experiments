0c21ed3c82756b8639cb86b21182095f7b41d0db
Jeff Santner <jeffreysantner@gmail.com>
2018-06-01 14:45:10 -0700

    general outline and structure

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ cantera.suppress_thermo_warnings()
 dirname = os.path.dirname(os.path.normpath(os.path.dirname(__file__)))
 sys.path.append(dirname)
 from utilities import ignition
+
+
+def fun1():
+    pass
+
+def fun2():
+    pass
+
+
+
+if __name__ == "__main__":
+    # This is the code that will run when you call this file.
+    fun1()
+    fun2()
