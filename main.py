9b7ccfbc80059099cd913ebd0044e3a3ce116de3
dlopez5 <dlopez5@calstatela.edu>
2018-06-20 13:23:55 -0700

    testing
    testing

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ import cantera
 cantera.suppress_thermo_warnings()
 
 # import ignition tools from utilitites repository
-dirname = os.path.dirname(os.path.normpath(os.path.dirname(__file__)))
+dirname=os.path.dirname(os.path.abspath(os.path.curdir))
+dirname=dirname+'/utilities'
 sys.path.append(dirname)
-from utilities import ignition
+import ignition
 
+#create gas object
+gas1 = cantera.Solution('gri30.xml')
+
+#set state
+gas1.TP = 800, 8*101325   
+
+#set gas compsotion; X - mole fraction;  Y - mass fraction
+gas1.X = 'CH4:0.5, O2:2, N2:7.5'
+
+ignition.run(self)
 
 def fun1():
     pass
