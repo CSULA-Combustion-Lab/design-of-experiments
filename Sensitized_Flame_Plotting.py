9d5ae40860d91959e704cc6bf57c7027590a9618
Kodo <cising@calstatela.edu>
2020-04-08 10:44:12 -0700

    Comments and Name Change
    function rxn_interest_plots currently not being used. Commented out function to be worked on at a later time.
    
    Change names of varaibles in main with a capital letter and variables in the functions with a lower case letter

--- Sensitized_Flame_Plotting.py
+++ Sensitized_Flame_Plotting.py
@@ -12,14 +12,14 @@ import os
 import matplotlib.patches as mpl_patches
 #from tkinter import filedialog
 
-def rxn_plots(F_info, save_path):
+def rxn_plots(f_info, save_path):
     """[Insert Information]"""
-    Tint     = F_info[0]['Conditions'][3]
+    Tint     = f_info[0]['Conditions'][3]
     Pressure = []
     Fuel     = []
     Phi      = []
     Max_rxn  = []
-    for f in F_info:
+    for f in f_info:
         max_sens = 0
         max_num  = 0
         for m in f['Flame'][0]:
@@ -54,45 +54,57 @@ def rxn_plots(F_info, save_path):
         fig.suptitle('Initial Temperature: '+format(Tint)+' [K]')
         fig.tight_layout(rect=[0, 0.03, 1, 0.95])
         plt.savefig(save_path+'\\Max Reactions with Initial Temperature '
-                    +format(Tint)+' [K].png')
+                    +format(Tint)+'K.png')
     plt.close(fig)
     
     
-def rxn_strength_plots(F_info, rxn_int, nrxns):
+def rxn_strength_plots(f_info, rxn_int, nrxns, threshold, save_path):
     """[Insert Information]"""
-    sens_strength = []
-    average_nrxns = rxn_average(F_info, nrxns)
-    for f in F_info:
-        flame_sens = []
-        for fs in nrxns:
-            flame_sens.append()
-        flame_info = {'Sensitivity': flame_sens,
-                      'Conditions': flame['Conditions']}
-        flame_strength.append(flame_info)
-    
-def rxn_interest_plots(F_info, rxn_int, save_path):
-    """[Insert Information]"""
+    Tint     = f_info[0]['Conditions'][3]
     Pressure = []
     Fuel     = []
     Phi      = []
-    Tint     = F_info[0]['Conditions'][3]
-    Rxn_name = F_info[0]['Flame'][0][rxn_int][2]
-    Rxn_num  = F_info[0]['Flame'][0][rxn_int][0]
-    for f in F_info:
+    sens_strength = []
+    for f in f_info:
+        average_nrxns = rxn_average(f, nrxns)
+        strength = f['Flame'][0][rxn_int][1]/average_nrxns
+        sens_strength.append(strength)
         Pressure.append(f['Conditions'][0])
         Fuel.append(f['Conditions'][1])
         Phi.append(f['Conditions'][2])
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 10))
-    ax        = axes.flatten()
-    fs        = 15
+    figa, axesa = plt.subplots(nrows=1, ncols=3, figsize=(15, 10), sharey=True)
+    axa         = axesa.flatten()
+    fs          = 15
     #Dictionary organized as follow 'Key': [Data, axis-label]
     cond_dict = {'P': [Pressure, 'Pressure [atm]'],
                  'F': [Fuel, 'Fuel [Mole Fraction]'],
                  'Phi':[Phi, 'Equivalence Ratio [$\phi$]'],
-                 'T': [Tint, 'Temperature [K]']}
-    conditions = [('P', 'F'), ('P', 'Phi'), ('F', 'Phi')]
-    #Three subplots of unique paired independent variables
-    for a, condition in zip(ax, conditions):
+                 'T': [Tint, 'Temperature [K]'],
+                 'Strength': [sens_strength, 'Rxn Strength']}
+    conditions_a = ['P', 'F', 'Phi']
+    for a, condition in zip(axa, conditions_a):
+        x_key = condition
+        y_key = 'Strength'
+        a.plot(cond_dict[x_key][0], cond_dict[y_key][0], ls='none',
+               marker='o', mfc='none', mec='k')
+        a.set_xlabel(cond_dict[x_key][1], fontsize=fs)
+        a.set_ylabel(cond_dict[y_key][1], fontsize=fs)
+        a.set_xscale('log')
+        a.grid(True)
+
+        figa.suptitle('Reaction '+format(f_info[0]['Flame'][0][rxn_int][2])+
+                      '\nInitial Temperature: '+format(Tint)+' [K]')
+        figa.tight_layout(rect=[0, 0.03, 1, 0.95])
+        plt.savefig(save_path+'\\Reaction '
+                    +format(f_info[0]['Flame'][0][rxn_int][0])+
+                    ' Average Strength against top '+format(nrxns)+' rxns'
+                    ' with Initial Temperature '+format(Tint)+'K.png')
+    plt.close(figa)
+    
+    figb, axesb = plt.subplots(nrows=1, ncols=3, figsize=(15, 10))
+    axb         = axesb.flatten()
+    conditions_b = [('P', 'F'), ('P', 'Phi'), ('F', 'Phi')]
+    for a, condition in zip(axb, conditions_b):
         x_key = condition[0]
         y_key = condition[1]
         a.plot(cond_dict[x_key][0], cond_dict[y_key][0], ls='none',
@@ -100,25 +112,70 @@ def rxn_interest_plots(F_info, rxn_int, save_path):
         a.set_xlabel(cond_dict[x_key][1], fontsize=fs)
         a.set_ylabel(cond_dict[y_key][1], fontsize=fs)
         a.set_xscale('log')
-        a.set_yscale('log')
         a.grid(True)
 
-        fig.suptitle('Reaction Number: '+format(Rxn_num)+
-                      'Reaction Name: '+Rxn_name+
+        figa.suptitle('Reaction '+format(f_info[0]['Flame'][0][rxn_int][2])+
                       '\nInitial Temperature: '+format(Tint)+' [K]')
-        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-        plt.savefig(save_path+'\\Reaction Number '+format(Rxn_num)+
-                    ' Initial Temperature '+format(Tint)+' [K].png')
-    plt.close(fig)
+        figa.tight_layout(rect=[0, 0.03, 1, 0.95])
+        plt.savefig(save_path+'\\Reaction '
+                    +format(f_info[0]['Flame'][0][rxn_int][0])+
+                    ' Average Strength against top '+format(nrxns)+' rxns'
+                    ' with Initial Temperature '+format(Tint)+'K.png')
+    plt.close(figa)
+
 
 def rxn_average(flame, nrxns):
-    average_sens = []
+    """[Insert Information]"""
     rxn_sens     = []
-    for s in f['Flame'][0]:
-        rxns_sens.append(abs([s][1]))
-    rxns_sens.sort()
-    top_nrxns = rxns_sens[-nrxns:]
-    average.append(sum(top_nrxns)/len(top_nrxns))
+    for s in flame['Flame'][0]:
+        rxn_sens.append(abs(s[1]))
+    rxn_sens.sort()
+    top_nrxns = rxn_sens[-nrxns:]
+    average_sens = (sum(top_nrxns)/len(top_nrxns))
+    return average_sens
+ 
+    
+# def rxn_interest_plots(f_info, rxn_int, save_path):
+#     """[Insert Information]"""
+#     Pressure = []
+#     Fuel     = []
+#     Phi      = []
+#     Tint     = f_info[0]['Conditions'][3]
+#     Rxn_name = f_info[0]['Flame'][0][rxn_int][2]
+#     Rxn_num  = f_info[0]['Flame'][0][rxn_int][0]
+#     for f in f_info:
+#         Pressure.append(f['Conditions'][0])
+#         Fuel.append(f['Conditions'][1])
+#         Phi.append(f['Conditions'][2])
+#     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 10))
+#     ax        = axes.flatten()
+#     fs        = 15
+#     #Dictionary organized as follow 'Key': [Data, axis-label]
+#     cond_dict = {'P': [Pressure, 'Pressure [atm]'],
+#                  'F': [Fuel, 'Fuel [Mole Fraction]'],
+#                  'Phi':[Phi, 'Equivalence Ratio [$\phi$]'],
+#                  'T': [Tint, 'Temperature [K]']}
+#     conditions = [('P', 'F'), ('P', 'Phi'), ('F', 'Phi')]
+#     #Three subplots of unique paired independent variables
+#     for a, condition in zip(ax, conditions):
+#         x_key = condition[0]
+#         y_key = condition[1]
+#         a.plot(cond_dict[x_key][0], cond_dict[y_key][0], ls='none',
+#                marker='o', mfc='none', mec='k')
+#         a.set_xlabel(cond_dict[x_key][1], fontsize=fs)
+#         a.set_ylabel(cond_dict[y_key][1], fontsize=fs)
+#         a.set_xscale('log')
+#         a.set_yscale('log')
+#         a.grid(True)
+
+#         fig.suptitle('Reaction Number: '+format(Rxn_num)+
+#                       'Reaction Name: '+Rxn_name+
+#                       '\nInitial Temperature: '+format(Tint)+' [K]')
+#         fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+#         plt.savefig(save_path+'\\Reaction Number '+format(Rxn_num)+
+#                     ' Initial Temperature '+format(Tint)+'K.png')
+#     plt.close(fig)
+
     
 if __name__ == "__main__":
     Folder_name = input('Please type name of folder.'
@@ -136,27 +193,30 @@ if __name__ == "__main__":
     Plot_path = Load_path+'\\Flame_Sensitivity_Plots'
 
     #Open up text file with description of simulation
-    file = open(Load_path+'\\Case Description.txt','r')
-    Description = file.read()
-    file.close()
+    File = open(Load_path+'\\Case Description.txt','r')
+    Description = File.read()
+    File.close()
     print(Description)
 
     #Import flame file found in corresponding folder
     with open(os.path.join(Load_path, 'Flame Information.pkl'), 'rb') as f:
-        flame_info = pickle.load(f)
+        Flame_info = pickle.load(f)
         
     #Create two lists of flame and no_flame created from flame_info    
-    flame = []
-    no_flame = []
-    for x in flame_info:
+    Flame = []
+    No_flame = []
+    for x in Flame_info:
         if x['Flame'] == None:
-            no_flame.append(x)
+            No_flame.append(x)
         else:
-            flame.append(x)
+            Flame.append(x)
             
     #Note flame is a dictionary of {'Flame': [Flame_sens Su], 
     #                               'Conditions': [P, Fuel, Phi, Tin, Mix]}
     
     #Plot Functions
-    Rxn_Interest = 5 #Reaction number of the reaction of interest
-    rxn_plots(flame, Plot_path)
+    Rxn_interest = 5 #Reaction number of the reaction of interest
+    Nrxns        = 5 #Top n-reactions
+    Threshold    = 2 #Threshold for rxn_interst to be above in average strength
+    rxn_plots(Flame, Plot_path)
+    rxn_strength_plots(Flame, Rxn_interest, Nrxns, Threshold, Plot_path)
