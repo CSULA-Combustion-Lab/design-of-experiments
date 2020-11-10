d8bc7555a8a6a280f9d7241d80f75a2f012cb4e1
Kodo <cising@calstatela.edu>
2020-11-10 14:38:59 -0800

    Update to User Inputs
    The user inputs above the  'main' statement was updated similar to the 1D simulation case. This is for the purpose of preparing to use the initializer instea of individual codes. Work still needs to be completed in individual codes before the used of the initializer.

--- Simulation_Initializer.py
+++ Simulation_Initializer.py
@@ -5,7 +5,7 @@ Created on Wed Sep 16 13:32:37 2020
 @author: Kodo Bear
 """
 # import code_2_5
-import Sensitized_Flame_Experiment
+import Sensitized_Flame_Experiment as 1D
 # import BurnerSimulation
 
 #Initializer
@@ -15,6 +15,15 @@ import Sensitized_Flame_Experiment
 ## 1D Simulates an adiabatic flame tracking species before, at, and after flame
 ## Burner Simulates a planar flame stabalized at some distance from burner
 Simulation_Type = '1D'
+
+#Mixture Type (Debug, Custom, Oxi_Dil, Fue_Dil)
+#Provide one of the four types of mixtures into
+# variable mixture_type as a string
+#  Debug is used for simulating a single flame to check code
+#  Custom is under construction
+#  Oxi_Dil creates a mixture where the Diluent is a ratio of the Oxidizer used
+#  Fue_Dil creates a mixture where the Diluent is a ratio of the Fuel used
+Mixture_type = 'Oxi_Dil'
 
 #Parameters (Pressure, Equivalence, Temperature, Diluent Percentage)
 ## All three parameters are lists of three numbers
@@ -31,24 +40,45 @@ Equivalence        = [0.05, 1, 12]
 Temperature        = [300, 400, 2]
 Diluent_Percentage = [0.05, 0.95, 12]
 
-#Mechanism File
-Mechanism = 'Li_model.cti'
-Fuel      = 'CH3OH'
-Oxidizer  = 'O2'
-Diluent   = 'N2'
+#Set experiment parameters
+Mechanism = 'Li_model_modified_trioxane.cti' #Mechanism file
+
+#Initial temperature of unburned mixture
+Tint = 373 #Temperature [K]
+
+#Parameters for mixture (Fuel, Oxidizer, Diluent)
+# Fuel and Oxidizer can either be a single chemical string or multichemical list
+#  For multichemical follow structure ['Chemical1', % of Total, ...]
+#  The percentage following the chemical name should sum up to 1
+# Diluent should be a single chemical added to the mixture
+Fuel     = 'C3H6O3' #chemical formula of fuel
+# Fuel  = ['CH4', .50 , 'CH3OH', .50]
+Oxidizer = 'O2' #chemical formula of oxidizer
+# Oxidizer = ['O2', .35 , 'NO2', .65]
+Diluent  = 'N2' #chemical formula of diluent
+Air      = 'O2:1, N2:3.76' #chemical components for air as an oxidizer
+
+#Flame Conditions
+Mingrid   = 200
+Mul_soret = False
+Loglevel  = 0
+
+#True/False statements
+Save_files = True # If true, save files for plotting script
 
 if __name__ == "__main__":
-    Parameter_List = (Pressure, Equivalence, Temperature, Diluent_Percentage)
-    Mechanism_List = (Mechanism, Fuel, Oxidizer, Diluent)
     
-    if Simulation_Taype == '0D':
-        0D = 0DSimulation(Parameter_List, Mechanism_List)
-        
+    if Simulation_Type == '0D':
+        # 0D = 0DSimulation(Parameter_List, Mechanism_List)
+        print('Under-Construction!')
     elif Simulation_Type =='1D':
-        1D = 1DSimulation(Parameter_List, Mechanism_List)
-        
-    elif Simulation_Type = 'Burner':
-        Burner = BurnerSimulation(Parameter_List, Mechanism_List)
+        1D.run_flame_simulation(Mechanism, Array_type, Pressure, Equivalence,
+                                F_to_D, O_to_D, Tint, Fuel, Oxidizer, Diluent,
+                                Air, Mingrid, Mul_soret, Loglevel,
+                                Mixture_type, Save_files)
+    elif Simulation_Type == 'Burner':
+        # Burner = BurnerSimulation(Parameter_List, Mechanism_List)
+        print('Under Construction!')
     else:
         print('Error! Simulation_Type String Does Not Match!'+
               '\nMake sure string matches one of three options!')
